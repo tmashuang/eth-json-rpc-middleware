755764c36d2908307aa198dc579966dd17c3f4df
Thomas <thomas.b.huang@gmail.com>
2018-10-09 14:59:27 -0700

    Test

--- cache-utils.js
+++ cache-utils.js
@@ -58,10 +58,6 @@ function blockTagParamIndex (payload) {
     case 'eth_getBalance':
     case 'eth_getCode':
     case 'eth_getTransactionCount':
-    case 'eth_call':
-      return 1
-    // blockTag is at index 0
-    case 'eth_estimateGas':
     case 'eth_getBlockByNumber':
       return 0
     // there is no blockTag
